3107f2ee5601911d4a900e6bddb79e441e2b732e
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-01 14:38:03 +0530

    Remove asset copying script from backend tools

--- backend/tools/copyAssets.js
+++
@@ -1,7 +0,0 @@
-import shell from "shelljs";
-
-// Copy all the view templates
-shell.cp("-R", "src/views", "dist/");
-// Copy all the static assets
-shell.cp("-R", "src/public/*.css", "dist/public/");
-
