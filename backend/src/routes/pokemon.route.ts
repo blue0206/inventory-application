697fcf395efb8a7b5521b601eb3eda4e74ccd204
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 15:31:27 +0530

    Integrate controller methods to pokemon route for CRUD operations

--- backend/src/routes/pokemon.route.ts
+++ backend/src/routes/pokemon.route.ts
@@ -1,16 +1,23 @@
 import { Router } from 'express';
+import {
+    createPokemon,
+    getPokemon,
+    getPokemonById,
+    updatePokemon,
+    deletePokemon
+} from '../controllers/pokemon.controller';
 
 const pokemonRouter = Router();
 
 // Get all pokemon.
-pokemonRouter.get('/');
+pokemonRouter.get('/', getPokemon);
 // Create new pokemon.
-pokemonRouter.post('/');
+pokemonRouter.post('/', createPokemon);
 // Get pokemon by id.
-pokemonRouter.get('/:pokemonId');
+pokemonRouter.get('/:pokemonId', getPokemonById);
 // Update pokemon by id.
-pokemonRouter.put('/:pokemonId');
+pokemonRouter.put('/:pokemonId', updatePokemon);
 // Delete pokemon by id.
-pokemonRouter.delete('/:pokemonId');
+pokemonRouter.delete('/:pokemonId', deletePokemon);
 
 export default pokemonRouter;
