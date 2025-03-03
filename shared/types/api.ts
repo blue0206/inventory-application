acb0e37d28b4a35575d6fa47177d84812420198e
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 17:34:55 +0530

    Refactor API types to include Trainer, Pokemon, and Type from Prisma client

--- shared/types/api.ts
+++ shared/types/api.ts
@@ -1,5 +1,10 @@
-import { PrismaClient } from '@prisma/client';
+import { Trainer as TrainerType, Pokemon as PokemonType, Type as TypeInModel } from '@prisma/client';
+import { prisma } from '../prisma-client/prisma';
 
-const prisma = new PrismaClient();
+type Prisma = typeof prisma;
 
-export type Prisma = typeof prisma;
+export type Trainer = TrainerType;
+export type Pokemon = PokemonType;
+export type Type = TypeInModel;
+
+export type Trainers = Awaited<ReturnType<Prisma['trainer']['findMany']>>;
