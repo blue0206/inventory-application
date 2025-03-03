e61c1728c77d012ac731b2f190f5971168c4d8a6
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 15:34:03 +0530

    Remove unused index route and clean up route exports

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,8 @@
-import indexRouter from "./index.route";
 import trainerRouter from "./trainer.route";
 import pokemonRouter from "./pokemon.route";
 import typeRouter from "./type.route";
 
 export {
-    indexRouter,
     trainerRouter,
     pokemonRouter,
     typeRouter
