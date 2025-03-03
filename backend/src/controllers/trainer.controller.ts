bae94ee23c6a83d4d14c0c64755bcdfa428c6a40
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 23:47:29 +0530

    Implement getTrainers controller method with graceful error handling

--- backend/src/controllers/trainer.controller.ts
+++ backend/src/controllers/trainer.controller.ts
@@ -1,9 +1,30 @@
 import { Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
+import { prisma, Trainers, ApiResponse, NotFoundError } from 'shared';
 
-const createTrainer = asyncHandler(async (req: Request, res: Response) => {}) 
+const createTrainer = asyncHandler(async (req: Request, res: Response) => {
 
-const getTrainers = asyncHandler(async (req: Request, res: Response) => {}) 
+}) 
+
+const getTrainers = asyncHandler(async (req: Request, res: Response) => {
+    const trainers: Trainers = await prisma.trainer.findMany();
+
+    // Check if array is empty. If so, return a not found error.
+    if (trainers.length === 0) {
+        res.status(404).json(
+            new NotFoundError("Not Found", [{ message: "No trainers found." }])
+        );
+    }
+    // Return the list of trainers.
+    res.status(200).json(
+        new ApiResponse<Trainers>(
+            200, 
+            trainers, 
+            "Successfully retrieved all trainers.",
+            true
+        )
+    );
+});
 
 const getTrainerById = asyncHandler(async (req: Request, res: Response) => {}) 
 
