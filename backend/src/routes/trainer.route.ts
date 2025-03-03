3a772ad932ba8a13f6b917dce7bb57e31b8c4475
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 15:17:55 +0530

    Add routes for creating and updating trainers

--- backend/src/routes/trainer.route.ts
+++ backend/src/routes/trainer.route.ts
@@ -4,8 +4,12 @@ const trainerRouter = Router();
 
 // Get all trainers.
 trainerRouter.get('/');
+// Create new trainer.
+trainerRouter.post('/');
 // Get trainer by id.
 trainerRouter.get('/:trainerId');
+// Update trainer by id.
+trainerRouter.put('/:trainerId');
 // Delete trainer by id.
 trainerRouter.delete('/:trainerId');
 
