330b5b5b70c9696a43d654213c84b33d6b29cf04
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 13:51:13 +0530

    Add index route with POST and PUT operations

---
+++ backend/src/routes/index.route.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const indexRouter = Router();
+
+// Create Operation
+indexRouter.post('/');
+// Update Operation
+indexRouter.put('/');
+
+export default indexRouter;
