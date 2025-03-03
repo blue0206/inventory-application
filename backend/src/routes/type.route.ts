66371e8e0a98443b3bdc02f5f44f4ab922a50116
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 15:35:33 +0530

    Integrate controller methods to types route for CRUD operations

--- backend/src/routes/type.route.ts
+++ backend/src/routes/type.route.ts
@@ -1,16 +1,23 @@
 import { Router } from "express";
+import {
+    createType,
+    getTypes,
+    getTypeById,
+    updateType,
+    deleteType
+} from '../controllers/type.controller';
 
 const typeRouter = Router();
 
 // Get all types.
-typeRouter.get('/');
+typeRouter.get('/', getTypes);
 // Create new type.
-typeRouter.post('/');
+typeRouter.post('/', createType);
 // Get type by id.
-typeRouter.get('/:id');
+typeRouter.get('/:id', getTypeById);
 // Update type by id.
-typeRouter.put('/:id');
+typeRouter.put('/:id', updateType);
 // Delete type by id.
-typeRouter.delete('/:id');
+typeRouter.delete('/:id', deleteType);
 
 export default typeRouter;
