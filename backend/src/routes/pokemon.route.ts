62a0806a643d6b56fc034cf1c3d032cc757910e4
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 15:20:25 +0530

    Add routes for creating and updating pokemon

--- backend/src/routes/pokemon.route.ts
+++ backend/src/routes/pokemon.route.ts
@@ -4,8 +4,12 @@ const pokemonRouter = Router();
 
 // Get all pokemon.
 pokemonRouter.get('/');
+// Create new pokemon.
+pokemonRouter.post('/');
 // Get pokemon by id.
 pokemonRouter.get('/:pokemonId');
+// Update pokemon by id.
+pokemonRouter.put('/:pokemonId');
 // Delete pokemon by id.
 pokemonRouter.delete('/:pokemonId');
 
