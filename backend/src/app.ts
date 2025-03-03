9abe9068d8f999a9629ea40779d43fb59d2b1c4a
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 13:56:55 +0530

    Add trainer routes to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
 import indexRouter from './routes/index.route';
+import trainerRouter from './routes/trainer.route';
 
 const app = express();
 
 // Routes
 app.use('/', indexRouter);
+app.use('/trainers', trainerRouter);
 
 // Server
 const PORT = process.env.port || 8000;
