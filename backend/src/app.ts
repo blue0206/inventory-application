92b7f80cc6c370f82d1f14f879bcd6f03275a6be
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 15:39:32 +0530

    Add error middleware skeleton to setup error handling later

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'dotenv/config';
 import { trainerRouter, pokemonRouter, typeRouter } from './routes';
 
@@ -10,6 +10,9 @@ app.use('/api/v1/pokemon', pokemonRouter);
 app.use('/api/v1/types', typeRouter);
 // TODO: Give an error with code 404 if the route doesn't exist
 app.route('/api/v1/*');
+
+// TODO: Add a middleware to handle errors.
+app.use((err: Error, req: Request, res: Response) => {})
 
 // Server
 const PORT = process.env.port || 8000;
