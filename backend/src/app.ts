ebbc0c5381624033a0d695cf51e6f9a49bd6c59b
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 13:59:04 +0530

    Add Pokemon routes to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import 'dotenv/config';
 import indexRouter from './routes/index.route';
 import trainerRouter from './routes/trainer.route';
+import pokemonRouter from './routes/pokemon.route';
 
 const app = express();
 
 // Routes
 app.use('/', indexRouter);
 app.use('/trainers', trainerRouter);
+app.use('/pokemon', pokemonRouter);
+
 
 // Server
 const PORT = process.env.port || 8000;
