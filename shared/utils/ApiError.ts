4c5371b7adbf5294150a8b2a854bcc0aea1de978
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 23:30:32 +0530

    Add ApiError class to enhance error handling with status codes and serialization

--- shared/utils/ApiError.ts
+++ shared/utils/ApiError.ts
@@ -7,6 +7,34 @@ abstract class CustomError extends Error {
     }
 }
 
+export class ApiError extends CustomError {
+    statusCode: number;
+    success: boolean;
+    errors: ApiErrorList;
+    constructor(
+        statusCode: number, 
+        message: string="Something went wrong.", 
+        errors: ApiErrorList=[], 
+        stack?: string, 
+        success?: boolean
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.errors = errors;
+        this.success = success || false;
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+
+    serializeErrors(): ApiErrorList {
+        return this.errors;
+    }
+}
+
 type ApiErrorList = Array<{
     message: string;
     field?: string;
