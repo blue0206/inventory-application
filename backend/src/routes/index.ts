c28af921018a9f01de7303d6e7800d25564bb6f6
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 14:16:10 +0530

    Re-export routes from index

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,11 @@
+import indexRouter from "./index.route";
+import trainerRouter from "./trainer.route";
+import pokemonRouter from "./pokemon.route";
+import typeRouter from "./type.route";
+
+export {
+    indexRouter,
+    trainerRouter,
+    pokemonRouter,
+    typeRouter
+}
