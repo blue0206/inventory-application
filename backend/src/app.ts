57a652848c432dbcaf85ffd15b6ac4af2d2a69fa
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-03 13:52:50 +0530

    Add index route to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
+import indexRouter from './routes/index.route';
 
 const app = express();
 
+// Routes
+app.use('/', indexRouter);
+
+// Server
 const PORT = process.env.port || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}(http://localhost:${PORT})`);
