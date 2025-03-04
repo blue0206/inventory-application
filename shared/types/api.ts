a65b75b1f6148f0cda490f523292725fddd1ac48
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-05 00:45:16 +0530

    Add request body types for trainer and pokemon creation

--- shared/types/api.ts
+++ shared/types/api.ts
@@ -1,10 +1,28 @@
 import { Trainer as TrainerType, Pokemon as PokemonType, Type as TypeInModel } from '@prisma/client';
 import { prisma } from '../prisma-client/prisma';
 
+// Prisma Client Type
 type Prisma = typeof prisma;
 
+// Model Types
 export type Trainer = TrainerType;
 export type Pokemon = PokemonType;
 export type Type = TypeInModel;
 
+// Query Types
 export type Trainers = Awaited<ReturnType<Prisma['trainer']['findMany']>>;
+
+// Request Body Types
+export interface CreateTrainerRequestBody {
+	trainerName: string;
+    trainerImage: string;
+    pokemonList: CreatePokemonRequestBody[];
+}
+export interface CreatePokemonRequestBody {
+    pokemonName: string;
+    pokemonImage: string;
+    pokemonTypes: CreateTypeRequestBody[];
+}
+export interface CreateTypeRequestBody {
+    typeName: string;
+}
