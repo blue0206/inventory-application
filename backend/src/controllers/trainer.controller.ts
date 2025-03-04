e4c8a79a99e0a68e0ba9f2bdfceeb1fc27a6826c
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-04 23:50:24 +0530

    Implement getTrainerById controller method with error handling
    - Implement getTrainerById controller method
    - Add error handling for missing and invalid trainerId

--- backend/src/controllers/trainer.controller.ts
+++ backend/src/controllers/trainer.controller.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
-import { prisma, Trainers, ApiResponse, NotFoundError, BadRequestError } from 'shared';
+import { 
+    prisma, 
+    Trainer,
+    Trainers, 
+    ApiResponse, 
+    NotFoundError, 
+    BadRequestError 
+} from 'shared';
 
 const createTrainer = asyncHandler(async (req: Request, res: Response) => {
 
@@ -24,7 +31,37 @@ const getTrainers = asyncHandler(async (req: Request, res: Response) => {
     );
 });
 
-const getTrainerById = asyncHandler(async (req: Request, res: Response) => {}) 
+const getTrainerById = asyncHandler(async (req: Request, res: Response) => {
+    // Get trainer id from request params.
+    const { trainerId } = req.params;
+    // Check if trainerId exists.
+    if (!trainerId.trim()) {
+        throw new BadRequestError("Bad Request", [{ message: "Missing required parameter: trainerId" }]);
+    }
+    // Check if trainerId is a number.
+    if (isNaN(Number(trainerId))) {
+        throw new BadRequestError("Bad Request", [{ message: "trainerId must be a number" }]);
+    }
+    // Get trainer with trainerId from database.
+    const trainer: Trainer | null = await prisma.trainer.findUnique({
+        where: {
+            id: Number(trainerId)
+        }
+    });
+    // Check if trainer was found.
+    if (!trainer) {
+        throw new NotFoundError("Not Found", [{ message: "No trainers found." }]);
+    }
+    // Return trainer.
+    res.status(200).json(
+        new ApiResponse<Trainer>(
+            200,
+            trainer,
+            `Successfully retrieved trainer ${trainer.name}.`,
+            true
+        )
+    );
+});
 
 const updateTrainer = asyncHandler(async (req: Request, res: Response) => {}) 
 
