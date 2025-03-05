aba1c7e2920df8a63c7f2d49b37b585291595264
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-05 17:58:49 +0530

    Update request body types to make trainerImage and pokemonImage optional

--- shared/types/api.ts
+++ shared/types/api.ts
@@ -2,28 +2,25 @@ import { Trainer as TrainerType, Pokemon as PokemonType, Type as TypeInModel } f
 import { prisma } from '../prisma-client/prisma';
 
 // Prisma Client Type
-// Prisma Client Type
 type Prisma = typeof prisma;
 
-// Model Types
 // Model Types
 export type Trainer = TrainerType;
 export type Pokemon = PokemonType;
 export type Type = TypeInModel;
 
 // Query Types
-// Query Types
 export type Trainers = Awaited<ReturnType<Prisma['trainer']['findMany']>>;
 
 // Request Body Types
 export interface CreateTrainerRequestBody {
 	trainerName: string;
-    trainerImage: string;
+    trainerImage?: string;
     pokemonList: CreatePokemonRequestBody[];
 }
 export interface CreatePokemonRequestBody {
     pokemonName: string;
-    pokemonImage: string;
+    pokemonImage?: string;
     pokemonTypes: CreateTypeRequestBody[];
 }
 export interface CreateTypeRequestBody {
