b580e764d724ef60f2362159cdee0cf327c4e257
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-07 11:57:34 +0530

    Add isCustomError to exports in shared/index.ts

--- shared/index.ts
+++ shared/index.ts
@@ -1,5 +1,5 @@
 export type { Trainer, Pokemon, PokeType, TrainerWithRelation, TrainerPokemon, TrainerRequestBody, PokemonRequestBody } from "./types/api.ts";
 export { prisma, Prisma } from "./prisma-client/prisma.js";
 export { ApiResponse } from "./utils/ApiResponse.js";
-export { ApiError, BadRequestError, NotFoundError, ValidationError } from "./utils/ApiError.js";
+export { ApiError, BadRequestError, NotFoundError, ValidationError, isCustomError } from "./utils/ApiError.js";
 export type { ApiErrorList } from "./utils/ApiError.ts"
