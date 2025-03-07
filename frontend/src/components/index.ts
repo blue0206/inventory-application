fd70c025d004df15b7f1325609441860215f1ed0
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-08 00:08:40 +0530

    Add index file to export all components

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,17 @@
+import TrainersList from "./Trainers/TrainersList";
+import Trainer from "./Trainers/Trainer";
+import TrainerForm from "./Trainers/TrainerForm";
+import PokemonList from "./Pokemon/PokemonList";
+import Pokemon from "./Pokemon/Pokemon";
+import PokemonForm from "./Pokemon/PokemonForm";
+import NotFound from "./NotFound";
+
+export {
+    TrainersList,
+    Trainer,
+    TrainerForm,
+    PokemonList,
+    Pokemon,
+    PokemonForm,
+    NotFound
+}
