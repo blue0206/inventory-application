78e3f59a85697111b01733a3b918b3a859f2d6b2
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-07 12:52:03 +0530

    Implement error handling middleware to manage custom errors and Prisma client errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import 'dotenv/config';
 import { trainerRouter, pokemonRouter } from './routes/index.js';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
-import { BadRequestError, NotFoundError } from 'shared';
+import { ApiErrorList, ApiResponse, isCustomError, NotFoundError } from 'shared';
 
 const app = express();
 // Middlewares
@@ -16,8 +16,37 @@ app.use("/*", (req: Request, res: Response) => {
   throw new NotFoundError("Not Found");
 });
 
-// TODO: Add a middleware to handle errors.
-app.use((err: Error, req: Request, res: Response) => {})
+// Error Middleware
+app.use((err: Error, req: Request, res: Response) => {
+    if (isCustomError(err)) {
+        res.status(err.statusCode).json(
+            new ApiResponse<ApiErrorList>(
+                err.statusCode,
+                err.serializeErrors(),
+                err.message,
+                err.success
+            )
+        );
+    } else if (err instanceof PrismaClientKnownRequestError) {
+        res.status(400).json(
+            new ApiResponse<Record<string, unknown>>(
+                409,
+                err.meta,
+                "The data already exists.",
+                false
+            )
+        );
+    } else {
+        res.status(500).json(
+            new ApiResponse<null>(
+                500,
+                null,
+                "Internal server error",
+                false
+            )
+        );
+    }
+});
 
 // Server
 const PORT = process.env.port || 8000;
