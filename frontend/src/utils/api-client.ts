28a763dd8608b05867f06abee8570cdd0085ac29
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-09 15:38:33 +0530

    Add ApiClient utility for handling API requests and responses

---
+++ frontend/src/utils/api-client.ts
@@ -0,0 +1,53 @@
+import type { 
+    TrainerRequestBody, 
+    PokemonRequestBody,
+    ApiErrorList 
+} from "shared";
+import {
+    ApiErrorResponse,
+    ApiResponse
+} from "shared";
+
+class ApiClient {
+    private async fetch<DataType>(
+        endpoint: string,
+        options: FetchOptions
+    ): Promise<DataType> {
+        const { method = "GET", body, headers }: FetchOptions = options;
+
+        const response: Response = await fetch(`${import.meta.env.VITE_API_BASE_URL}${endpoint}`, {
+            method,
+            body: body ? JSON.stringify(body) : undefined,
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+
+        const data: 
+            | ApiResponse<DataType> 
+            | ApiErrorResponse<ApiErrorList | Record<string, unknown> | void> = await response.json();
+
+        if ('error' in data) {
+            if (data.statusCode === 500) {
+                throw new Error("Internal Server Error");
+            } else {
+                throw new Error(data.message);
+            }
+        }
+
+        return data.data;
+    }
+}
+
+type FetchOptions = {
+    method: "GET" | "POST" | "PUT" | "DELETE";
+    body?:  TrainerRequestBody | PokemonRequestBody;
+    headers?: Record<string, string>;
+}
+
+export const apiClient = new ApiClient();
