ed86ca5e2e9e8a2e88ab143749258a7c65ff9fb9
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-09 15:51:40 +0530

    Retrieve secret key from query params instead of body
    Updated the auth middleware to retrieve the secret key from
    query parameters instead of the request body to comply with delete
    request standards. This change ensures that the middleware aligns with
    the expected behavior for HTTP DELETE requests.

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -3,11 +3,11 @@ import { UnauthorizedError } from 'shared/utils/ApiError.js';
 
 const verifySecretKey = (req: Request, res: Response, next: NextFunction) => {
     // Get the secret key from the request body.
-    const { secretKey } = req.body;
+    const { secretKey } = req.query;
     // Throw unauthorized request error if secret key is incorrect.
-    if (secretKey.trim().toLowerCase() !== process.env.SECRET_KEY) {
+    if (secretKey.toString().trim().toLowerCase() !== process.env.SECRET_KEY) {
         throw new UnauthorizedError("Unauthorized Request", [{ message: "The secret key is invalid", field: "secretKey" }]);
-    }
+    }  
     // If the secret key is correct, then continue to the next middleware.
     next();
 }
