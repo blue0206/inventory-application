9c720d1ed7687561e4a414c511fcee9d1b8129a0
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-10 23:23:47 +0530

    Add error and success middleware to Redux store configuration

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -2,13 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import trainerSlice from "../features/trainer/trainerSlice";
 import pokemonSlice from "../features/pokemon/pokemonSlice";
 import errorSlice from "../features/error/errorSlice";
+import { errorHandlingMiddleware } from "./middlewares/errorMiddleware";
+import { successMiddleware } from "./middlewares/successMiddleware";
 
 export const store = configureStore({
     reducer: {
         trainer: trainerSlice,
         pokemon: pokemonSlice,
         error: errorSlice
-    }
+    },
+    middleware: (getDefaultMiddleware) =>  getDefaultMiddleware().concat(
+                errorHandlingMiddleware,
+                successMiddleware
+            )
 });
 
 export type AppStore = typeof store;
