4c81f8403d79a4a6dc380ab3e2c1e02e0b68f351
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-10 17:48:26 +0530

    Add createAppAsyncThunk for typed async thunks in Redux

--- frontend/src/app/hooks.ts
+++ frontend/src/app/hooks.ts
@@ -1,5 +1,10 @@
 import { useSelector, useDispatch } from "react-redux";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState, AppDispatch } from "./store";
 
 export const useAppSelector = useSelector.withTypes<RootState>();
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const createAppAsyncThunk = createAsyncThunk.withTypes<{
+    state: RootState;
+    dispatch: AppDispatch;
+}>();
