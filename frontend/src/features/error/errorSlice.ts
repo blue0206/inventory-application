80d07b075baae103641fe7f901488e11e09557f0
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-10 12:53:45 +0530

    Enhance errorSlice to include error code and setError reducer

--- frontend/src/features/error/errorSlice.ts
+++ frontend/src/features/error/errorSlice.ts
@@ -1,23 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { ApiErrorTypes } from 'shared';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ApiErrorList } from 'shared';
+import { CustomError } from '../../utils/custom-error';
 
 type ErrorState = {
     hasError: boolean;
-    error: ApiErrorTypes | Record<string, unknown> | null;
+    error: ApiErrorList | Record<string, unknown> | null;
     message: string;
+    code: number;
 }
 
 const initialState: ErrorState = {
     hasError: false,
     error: null,
-    message: ''
+    message: '',
+    code: 200
 }
 
 const errorSlice = createSlice({
     name: 'error',
     initialState,
-    reducers: {}
+    reducers: {
+        setError: (state, action: PayloadAction<CustomError>) => {
+            state.hasError = true;
+            state.message = action.payload.message;
+            state.error = action.payload.error;
+            state.code = action.payload.statusCode;
+            return state;
+        }
+    }
 });
 
 export default errorSlice.reducer;
-export const {} = errorSlice.actions;
+export const {
+    setError
+} = errorSlice.actions;
