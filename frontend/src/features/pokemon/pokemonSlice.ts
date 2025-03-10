1d1f086ec4d4a15e23950f2cf5aa5df44b11b167
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-11 00:36:13 +0530

    Add async thunk for fetching Pokémon list and update slice state management

--- frontend/src/features/pokemon/pokemonSlice.ts
+++ frontend/src/features/pokemon/pokemonSlice.ts
@@ -1,12 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createAppAsyncThunk } from "../../app/hooks";
 import type { Pokemon } from 'shared';
+import { apiClient } from "../../utils/api-client";
 
-const initialState: Array<Pokemon> = [];
+export const fetchPokemonList = createAppAsyncThunk('pokemon/fetchPokemonList', async (_, { rejectWithValue }) => {
+    try {
+        const response = await apiClient.getPokemonList();
+        return response;
+        
+    } catch (error) {
+        rejectWithValue(error);
+    }
+});
+
+type PokemonState = {
+    status: 'idle' | 'loading' | 'succeeded' | 'failed';
+    pokemonList: Array<Pokemon>
+}
+
+const initialState: PokemonState = {
+    status: 'idle',
+    pokemonList: []
+};
 
 const pokemonSlice = createSlice({
     name: 'pokemon',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchPokemonList.pending, (state, action) => {
+            state.status = 'loading';
+        })
+        .addCase(fetchPokemonList.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+        })
+        .addCase(fetchPokemonList.rejected, (state, action) => {
+            state.status = 'failed';
+        })
+    }
 });
 
 export default pokemonSlice.reducer;
