7973942a729ca0a8e69fdd1f3938893b8638c71d
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-11 22:35:04 +0530

    Implement TrainerCard component with avatar, title, and action buttons

--- frontend/src/components/Trainers/TrainerCard.tsx
+++ frontend/src/components/Trainers/TrainerCard.tsx
@@ -1,7 +1,36 @@
 import { ReactElement } from 'react';
+import {
+    Card,
+    CardHeader,
+    CardContent,
+    CardFooter,
+    CardTitle
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Button } from "@/components/ui/button";
+import RedAvatar from "../../assets/red-avatar.png";
 
 export default function TrainerCard(): ReactElement {
     return (
-        <div></div>
+        <Card>
+            <CardHeader>
+                <AspectRatio ratio={16/9} className='rounded-lg'>
+                    <Avatar className='w-full h-full'>
+                        <AvatarImage src={RedAvatar} />
+                        <AvatarFallback>
+                            <img src={""} alt="Trainer Image" />
+                        </AvatarFallback>
+                    </Avatar>
+                </AspectRatio>
+            </CardHeader>
+            <CardContent>
+                <CardTitle>Name</CardTitle>
+            </CardContent>
+            <CardFooter>
+                <Button variant={"ghost"} className='cursor-pointer'>Update</Button>
+                <Button variant={"destructive"} className='cursor-pointer'>Delete</Button>
+            </CardFooter>
+        </Card>
     );
 }
