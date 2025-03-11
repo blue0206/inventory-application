f3ddc29511d4b82b9ce82e983957b03f444cb9d9
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-11 17:21:21 +0530

    Add layout and header to App component with hero content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
-function App() {
+import { ReactElement } from 'react';
+import { Header } from './components';
+
+function App(): ReactElement {
 
   return (
-    <></>
+    <>
+      <div className='h-screen w-full'>
+        <Header />
+        <div className='flex flex-col items-center justify-center h-full px-4 gap-3.5'>
+          <h1 className='text-8xl max-w-full'>This is Poké-Inventory.</h1>
+          <div>Explore</div>
+          <blockquote className='mt-6 border-l-2 pl-6 italic w-max max-w-full'>
+            <div>
+              There's nothing we could ever do that's a waste of time.
+            </div>
+            <div className='text-right'>— Ash Ketchum</div>
+          </blockquote>
+        </div>
+      </div>
+    </>
   )
 }
 
