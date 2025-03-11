732242bd028ce464b0026c44dd2cd2f8fa44bf88
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-11 14:51:20 +0530

    Implement Header component with nav menu for Home, Trainers, and Pokemon

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,44 @@
 import { ReactElement } from "react";
+import { NavLink } from "react-router";
+import {
+    NavigationMenu,
+    NavigationMenuList,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    navigationMenuTriggerStyle 
+} from './ui/navigation-menu';
 
 export default function Header(): ReactElement {
     return (
-        <></>
+        <>
+            <header className="w-full flex justify-between bg-white h-40">
+                <div>
+                    <nav>
+                        <NavigationMenu>
+                            <NavigationMenuList>
+                                <NavigationMenuItem>
+                                    <NavLink to={"/"}>
+                                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>Home</NavigationMenuLink>
+                                    </NavLink>
+                                </NavigationMenuItem>
+                                <NavigationMenuItem>
+                                    <NavLink to={"/trainers"}>
+                                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>Trainers</NavigationMenuLink>
+                                    </NavLink>
+                                </NavigationMenuItem>
+                                <NavigationMenuItem>
+                                    <NavLink to={"/pokemon"}>
+                                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>Pokemon</NavigationMenuLink>
+                                    </NavLink>
+                                </NavigationMenuItem>
+                            </NavigationMenuList>
+                        </NavigationMenu>
+                    </nav>
+                </div>
+                <div>
+
+                </div>
+            </header>
+        </>
     );
 }
