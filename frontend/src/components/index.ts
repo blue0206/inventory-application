b192c6ef700395987e6755883b7d91d05977d462
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-11 17:40:21 +0530

    Export TrainerCard and PokemonCard from components index

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,8 +1,10 @@
 import Header from "./Header/Header";
 import TrainersList from "./Trainers/TrainersList";
+import TrainerCard from "./Trainers/TrainerCard";
 import Trainer from "./Trainers/Trainer";
 import TrainerForm from "./Trainers/TrainerForm";
 import PokemonList from "./Pokemon/PokemonList";
+import PokemonCard from "./Pokemon/PokemonCard";
 import Pokemon from "./Pokemon/Pokemon";
 import PokemonForm from "./Pokemon/PokemonForm";
 import ErrorComponent from "./ErrorComponent";
@@ -11,9 +13,11 @@ import NotFound from "./NotFound";
 export {
     Header,
     TrainersList,
+    TrainerCard,
     Trainer,
     TrainerForm,
     PokemonList,
+    PokemonCard,
     Pokemon,
     PokemonForm,
     ErrorComponent,
