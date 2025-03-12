d35ad2671fe254a32bc0b67f01f6cc4dceaf5b43
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-12 13:45:09 +0530

    feat: Enhance fetchPokemonList with improved error handling and type def

--- frontend/src/features/pokemon/pokemonSlice.ts
+++ frontend/src/features/pokemon/pokemonSlice.ts
@@ -2,16 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { createAppAsyncThunk } from "../../app/hooks";
 import type { Pokemon } from 'shared';
 import { apiClient } from "../../utils/api-client";
-
-export const fetchPokemonList = createAppAsyncThunk('pokemon/fetchPokemonList', async (_, { rejectWithValue }) => {
-    try {
-        const response = await apiClient.getPokemonList();
-        return response;
-        
-    } catch (error) {
-        return rejectWithValue(error);
-    }
-});
+import { FetchError, isCustomDefinedError } from "../../utils/custom-error";
 
 type PokemonState = {
     status: 'idle' | 'loading' | 'succeeded' | 'failed';
@@ -29,18 +20,32 @@ const pokemonSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
-        .addCase(fetchPokemonList.pending, (state, action) => {
+        .addCase(fetchPokemonList.pending, (state) => {
             state.status = 'loading';
         })
-        .addCase(fetchPokemonList.fulfilled, (state, action) => {
+        .addCase(fetchPokemonList.fulfilled, (state) => {
             state.status = 'succeeded';
         })
-        .addCase(fetchPokemonList.rejected, (state, action) => {
+        .addCase(fetchPokemonList.rejected, (state) => {
             state.status = 'failed';
         })
     },
     selectors: {
         getPokemonList: (state: PokemonState) => state.pokemonList
+    }
+});
+
+export const fetchPokemonList = 
+createAppAsyncThunk<Array<Pokemon>>('pokemon/fetchPokemonList', async (_, { rejectWithValue }) => {
+    try {
+        const response: Array<Pokemon> = await apiClient.getPokemonList();
+        return response;     
+    } catch (error) {
+        // Actions shouldn't take class instances, hence destructure errors to object.
+        if (isCustomDefinedError(error)) {
+            return rejectWithValue({...error});
+        }
+        return rejectWithValue({...new FetchError("Something went wrong.")});
     }
 });
 
