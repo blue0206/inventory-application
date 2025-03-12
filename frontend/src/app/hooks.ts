6fa0aa56f15df24c5aa3a9d086f83a76392b550e
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-12 12:29:53 +0530

    fix: Provide correct rejectValue type for rejectValue in createAppAsyncThunk

--- frontend/src/app/hooks.ts
+++ frontend/src/app/hooks.ts
@@ -1,10 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState, AppDispatch } from "./store";
+import { CustomDefinedErrorType } from "../utils/custom-error";
 
 export const useAppSelector = useSelector.withTypes<RootState>();
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const createAppAsyncThunk = createAsyncThunk.withTypes<{
     state: RootState;
     dispatch: AppDispatch;
+    rejectValue: CustomDefinedErrorType;
 }>();
