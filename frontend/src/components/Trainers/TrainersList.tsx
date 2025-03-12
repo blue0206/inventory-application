74e64bc60748c7b7dfe42569b4d08826a2555067
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-12 13:18:33 +0530

    feat: Implement data fetching logic in TrainersList component

--- frontend/src/components/Trainers/TrainersList.tsx
+++ frontend/src/components/Trainers/TrainersList.tsx
@@ -1,6 +1,27 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
+import { useAppSelector, useAppDispatch } from "@/app/hooks";
+import { fetchTrainersList, resetStatus } from "@/features/trainer/trainerSlice";
 
 export default function TrainersList(): ReactElement {
+    const dispatch = useAppDispatch();
+    const status = useAppSelector(state => state.trainer.status);
+    
+    // TODO: Reset status to 'idle' when create/update route is
+    // implemented to re-fetch new and updated data.
+    useEffect(() => {
+        // Fetch trainers if status is 'idle'
+        if (status === 'idle') {
+            dispatch(fetchTrainersList());
+        }
+        // Cleanup function. Resets status to 'idle'
+        // if fetch fails, to try again.
+        return () => {
+            if (status === 'failed') {
+                dispatch(resetStatus());
+            }
+        }
+    }, [dispatch, status]);
+
     return (
         <></>
     );
