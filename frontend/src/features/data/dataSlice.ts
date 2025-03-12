9645a14a76cfbecca943916e8eae2cf9095448e7
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-12 15:18:03 +0530

    docs: Add explanation for dataSlice purpose and its handling of trainer/pokemon data fetch

--- frontend/src/features/data/dataSlice.ts
+++ frontend/src/features/data/dataSlice.ts
@@ -11,6 +11,19 @@ const initialState: DataState = {
     pokemonDataStatus: 'idle'
 }
 
+/**
+ * The purpose of this slice is to handle thunks regarding specific
+ * tasks like getting specific trainer/pokemon data in order to display
+ * them on a separate dynamic route. The actual data pertaining to 
+ * trainer/pokemon is not required to be globally available and hence there
+ * is no Redux state created to hold them.
+ * 
+ * The reason this slice has been created and the fetching of data is not
+ * being handled in local component is because the error handling
+ * and notifications showcase has been standardized and centralized via redux 
+ * middlewares and to trigger that, a request made via async thunk
+ * is imperative.
+ */
 const dataSlice = createSlice({
     name: 'data',
     initialState,
