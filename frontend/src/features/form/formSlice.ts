e8c31cd5bcdbddf936396e419f8df608d6857290
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-12 15:08:35 +0530

    docs: Explain the purpose of choosing formSlice over local component management

--- frontend/src/features/form/formSlice.ts
+++ frontend/src/features/form/formSlice.ts
@@ -10,6 +10,18 @@ const initialState: FormState = {
     pokemonFormStatus: 'idle'
 }
 
+/**
+ * The purpose of this slice is to handle thunks regarding specific
+ * tasks like creating/updating trainer/pokemon. The actual data
+ * pertaining to trainer/pokemon is not required and hence there
+ * is no state created to hold them.
+ * 
+ * The reason this slice has been created and the form data is not
+ * being handled in local component is because the error handling
+ * and notifications showcase has been standardized via redux 
+ * middlewares and to trigger that, a request made via async thunk
+ * is imperative.
+ */
 export const formSlice = createSlice({
     name: 'form',
     initialState,
