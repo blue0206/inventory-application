8c7edeea987e0df8fe4745c63f50225058fb8f85
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-12 12:53:29 +0530

    feat: Integrate Redux Provider into main application entry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux';
+import { store } from "./app/store";
 import { BrowserRouter } from 'react-router';
 import AppRoutes from './routes';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
