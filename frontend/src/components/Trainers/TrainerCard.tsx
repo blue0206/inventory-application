b5b96d2087930b73522b7e0a16aba7d7e0c3d41e
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-12 23:22:24 +0530

    feat: Add confirmation dialog for deleting TrainerCard entries

--- frontend/src/components/Trainers/TrainerCard.tsx
+++ frontend/src/components/Trainers/TrainerCard.tsx
@@ -10,6 +10,16 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogFooter,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import RedAvatar from "../../assets/red-avatar.png";
 
 type TrainerCardProps = {
@@ -30,7 +40,7 @@ export default function TrainerCard({
 
     return (
         <Card className=' shadow-md hover:scale-105'>
-            <CardHeader className='cursor-pointer'>
+            <CardHeader>
                 <AspectRatio ratio={1.25/1}>
                     <Avatar className='w-full h-full'>
                         <AvatarImage className='h-full w-full object-scale-down' src={image ? image : ""} />
@@ -42,12 +52,33 @@ export default function TrainerCard({
                     </Avatar>
                 </AspectRatio>
             </CardHeader>
-            <CardContent className='cursor-pointer'>
+            <CardContent>
                 <CardTitle className='text-center text-lg'>{name}</CardTitle>
             </CardContent>
             <CardFooter className='flex flex-row gap-5 justify-center items-center'>
-                <Button variant={"link"} onClick={handleOpen} className='cursor-pointer hover:bg-accent'>Open</Button>
-                <Button variant={"destructive"} className='cursor-pointer'>Delete</Button>
+                <Button variant={"link"} onClick={handleOpen} className='cursor-pointer border-accent border-2 hover:bg-accent'>Open</Button>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant={"destructive"} className='cursor-pointer'>Delete</Button>
+                    </DialogTrigger>
+                    <DialogContent className="sm:max-w-[425px] gap-0 px-4 py-4">
+                        <DialogHeader>
+                            <DialogTitle className='text-lg'>Are you absolutely sure?</DialogTitle>
+                            <DialogDescription className='mb-3.5'>
+                                This action cannot be undone. This will permanently delete the entry 
+                                and remove the data from the server.
+                            </DialogDescription>
+                        </DialogHeader>
+                        <div className='flex flex-col gap-0.5 mb-1'>
+                            <p className='text-sm font-medium mb-1.5'>Enter secret key to proceed:</p>
+                            <Input type={'password'} />
+                            <div className='text-xs'>(Hint: Kazuma's sword)</div>
+                        </div>
+                        <DialogFooter>
+                            <Button variant={'destructive'} className='cursor-pointer'>Delete</Button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
             </CardFooter>
         </Card>
     );
