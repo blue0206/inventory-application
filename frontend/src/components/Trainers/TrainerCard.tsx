ce2078f2bf8df64b40fe2f93dc1f2a2838ef6c64
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-12 18:27:30 +0530

    refactor: Replace Update button with Open to navigate to trainer details page

--- frontend/src/components/Trainers/TrainerCard.tsx
+++ frontend/src/components/Trainers/TrainerCard.tsx
@@ -23,13 +23,14 @@ export default function TrainerCard({
     name,
     image
 }: TrainerCardProps): ReactElement {
-    const handleCardClick = () => {
+
+    const handleOpen = () => {
         navigationService.navigate(`/trainers/${id}/`);
     }
 
     return (
         <Card className=' shadow-md hover:scale-105'>
-            <CardHeader className='cursor-pointer' onClick={handleCardClick}>
+            <CardHeader className='cursor-pointer'>
                 <AspectRatio ratio={1.25/1}>
                     <Avatar className='w-full h-full'>
                         <AvatarImage className='h-full w-full object-scale-down' src={image ? image : ""} />
@@ -41,11 +42,11 @@ export default function TrainerCard({
                     </Avatar>
                 </AspectRatio>
             </CardHeader>
-            <CardContent className='cursor-pointer' onClick={handleCardClick}>
+            <CardContent className='cursor-pointer'>
                 <CardTitle className='text-center text-lg'>{name}</CardTitle>
             </CardContent>
             <CardFooter className='flex flex-row gap-5 justify-center items-center'>
-                <Button variant={"ghost"} className='cursor-pointer'>Update</Button>
+                <Button variant={"link"} onClick={handleOpen} className='cursor-pointer hover:bg-accent'>Open</Button>
                 <Button variant={"destructive"} className='cursor-pointer'>Delete</Button>
             </CardFooter>
         </Card>
