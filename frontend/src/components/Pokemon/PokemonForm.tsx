2155b6790e7b3154aa20c22f7c2c9568d1827a46
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-13 17:42:06 +0530

    feat: Add breadcrumb navigation to PokémonForm component

--- frontend/src/components/Pokemon/PokemonForm.tsx
+++ frontend/src/components/Pokemon/PokemonForm.tsx
@@ -1,7 +1,39 @@
 import { ReactElement } from "react";
+import { Header } from "..";
+import {
+    Breadcrumb,
+    BreadcrumbList,
+    BreadcrumbLink,
+    BreadcrumbPage,
+    BreadcrumbItem,
+    BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
+import { navigationService } from "../../utils/navigation";
 
 export default function PokemonForm(): ReactElement {
     return (
-        <></>
+        <div className="flex flex-col gap-2.5 h-full w-full">
+            <Header />
+            <div className="px-8">
+                <Breadcrumb>
+                    <BreadcrumbList>
+                        <BreadcrumbItem>
+                            <BreadcrumbLink className="cursor-pointer" onClick={() => navigationService.navigate("/")}>Home</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                            <BreadcrumbLink className="cursor-pointer" onClick={() => navigationService.navigate("/pokemon")}>Pokémon</BreadcrumbLink>
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator />
+                        <BreadcrumbItem>
+                            <BreadcrumbPage>Create Pokémon</BreadcrumbPage>
+                        </BreadcrumbItem>
+                    </BreadcrumbList>
+                </Breadcrumb>
+            </div>
+            <div>
+                
+            </div>
+        </div>
     );
 }
