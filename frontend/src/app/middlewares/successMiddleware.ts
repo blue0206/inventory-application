d4f757c054eb9fe168c9f641f48a985c71c35730
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-13 08:24:06 +0530

    feat: Add success notification for fetching all trainers in success middleware

--- frontend/src/app/middlewares/successMiddleware.ts
+++ frontend/src/app/middlewares/successMiddleware.ts
@@ -1,4 +1,5 @@
 import { isFulfilled, Middleware } from "@reduxjs/toolkit";
+import { toast } from "sonner";
 
 // Middleware for showing customized notifications when an action is successful
 // and to handle any other side effects.
@@ -11,6 +12,17 @@ export const successMiddleware: Middleware = () => (next) => (action) => {
          * Another purpose is to handle any side effects that need to happen after an
          * action has been completed like redirection, other component state updates, etc.
          */
+        switch (action.type) {
+            // Action fetching all trainers.
+            case "trainer/fetchTrainers/fulfilled":
+                // Display a success notification.
+                toast.success("Fetched all trainers successfully!", {
+                    duration: 3500
+                });
+                break;
+            default:
+                
+        }
     }
     return next(action);
 }
