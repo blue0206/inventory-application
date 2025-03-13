87498f4f71e2070601830935d8e4f651639aa0eb
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-13 14:39:43 +0530

    feat: Implement responsive delete confirmation using Dialog and Drawer

--- frontend/src/components/Trainers/TrainerCard.tsx
+++ frontend/src/components/Trainers/TrainerCard.tsx
@@ -19,6 +19,14 @@ import {
     DialogFooter,
     DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+    Drawer,
+    DrawerTrigger,
+    DrawerContent,
+    DrawerHeader,
+    DrawerTitle
+} from "@/components/ui/drawer";
+import { useMediaQuery } from "@custom-react-hooks/use-media-query";
 import { Input } from "@/components/ui/input";
 import RedAvatar from "../../assets/red-avatar.png";
 import { deleteTrainer } from '../../features/data/dataSlice';
@@ -37,13 +45,16 @@ export default function TrainerCard({
 }: TrainerCardProps): ReactElement {
     const dispatch = useAppDispatch();
     const [secretKey, setSecretKey] = useState<string>("");
+    // Check if screen is desktop. This will be used to render
+    // dialog for delete modal on desktop and drawer on mobile.
+    const isDesktop = useMediaQuery("(min-width: 768px)");
 
     const handleDelete = async () => {
         await dispatch(deleteTrainer({ id, secretKey}));
     }
     
     const handleOpen = () => {
-        navigationService.navigate(`/trainers/${id}/`);
+        navigationService.navigate(`/trainers/${id}`);
     }
 
     return (
@@ -65,28 +76,57 @@ export default function TrainerCard({
             </CardContent>
             <CardFooter className='flex flex-row gap-5 justify-center items-center'>
                 <Button variant={"link"} onClick={handleOpen} className='cursor-pointer border-accent border-2 hover:bg-accent'>Open</Button>
-                <Dialog>
-                    <DialogTrigger asChild>
-                        <Button variant={"destructive"} className='cursor-pointer'>Delete</Button>
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-[425px] gap-0 px-4 py-4">
-                        <DialogHeader>
-                            <DialogTitle className='text-lg'>Are you absolutely sure?</DialogTitle>
-                            <DialogDescription className='mb-3.5'>
-                                This action cannot be undone. This will permanently delete the entry 
-                                and remove the data from the server.
-                            </DialogDescription>
-                        </DialogHeader>
-                        <div className='flex flex-col gap-0.5 mb-1'>
-                            <p className='text-sm font-medium mb-1.5'>Enter secret key to proceed:</p>
-                            <Input type={'password'} value={secretKey} onChange={(e) => setSecretKey(e.target.value)}/>
-                            <div className='text-xs'>(Hint: Kazuma's sword)</div>
-                        </div>
-                        <DialogFooter>
-                            <Button variant={'destructive'} onClick={handleDelete} className='cursor-pointer'>Delete</Button>
-                        </DialogFooter>
-                    </DialogContent>
-                </Dialog>
+                {
+                    isDesktop ? (
+                        <Dialog>
+                            <DialogTrigger asChild>
+                                <Button variant={"destructive"} className='cursor-pointer'>Delete</Button>
+                            </DialogTrigger>
+                            <DialogContent className="sm:max-w-[425px] gap-0 px-4 py-4">
+                                <DialogHeader>
+                                    <DialogTitle className='text-lg'>Are you absolutely sure?</DialogTitle>
+                                    <DialogDescription className='mb-3.5'>
+                                        This action cannot be undone. This will permanently delete the trainer 
+                                        and remove the data from the server.
+                                    </DialogDescription>
+                                </DialogHeader>
+                                <div className='flex flex-col gap-0.5 mb-1'>
+                                    <p className='text-sm font-medium mb-1.5'>Enter secret key to proceed:</p>
+                                    <Input type={'password'} value={secretKey} onChange={(e) => setSecretKey(e.target.value)}/>
+                                    <div className='text-xs'>(Hint: Kazuma's sword)</div>
+                                </div>
+                                <DialogFooter>
+                                    <Button variant={'destructive'} onClick={handleDelete} className='cursor-pointer'>Delete</Button>
+                                </DialogFooter>
+                            </DialogContent>
+                        </Dialog>
+                    ) : (
+                        <Drawer>
+                            <DrawerTrigger asChild>
+                                <Button variant={"destructive"} className='cursor-pointer'>Delete</Button>
+                            </DrawerTrigger>
+                            <DrawerContent className='px-3.5 py-4'>
+                                <div className='mx-auto w-full max-w-sm'>
+                                    <DrawerHeader>
+                                        <DrawerTitle className='text-lg'>Are you absolutely sure?</DrawerTitle>
+                                        <DialogDescription className='mb-3.5'>
+                                            This action cannot be undone. This will permanently delete the trainer
+                                            and remove the data from the server.
+                                        </DialogDescription>
+                                    </DrawerHeader>
+                                    <div className='flex flex-col gap-0.5 mb-1'>
+                                        <p className='text-sm font-medium mb-1.5'>Enter secret key to proceed:</p>
+                                        <Input type={'password'} value={secretKey} onChange={(e) => setSecretKey(e.target.value)} />
+                                        <div className='text-xs'>(Hint: Kazuma's sword)</div>
+                                    </div>
+                                    <DialogFooter>
+                                        <Button variant={'destructive'} onClick={handleDelete} className='cursor-pointer'>Delete</Button>
+                                    </DialogFooter>
+                                </div>
+                            </DrawerContent>
+                        </Drawer>
+                    )
+                }
             </CardFooter>
         </Card>
     );
