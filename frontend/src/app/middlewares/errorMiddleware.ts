2f71cd7fe2cc55b2f98b05ac3aa621c190833492
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-13 07:33:33 +0530

    feat: Implement basic structure for centralized error handling middleware

--- frontend/src/app/middlewares/errorMiddleware.ts
+++ frontend/src/app/middlewares/errorMiddleware.ts
@@ -1,8 +1,25 @@
-import { AppStore } from "../store";
-import { Action, Middleware } from "@reduxjs/toolkit"
+import { isRejected, Middleware } from "@reduxjs/toolkit"
+import { isCustomDefinedError } from "@/utils/custom-error";
 
-export const errorHandlingMiddleware = (store: AppStore) => (next: Middleware) => (action: Action) => {
-    if (action.type.endsWith('/rejected')) {
-        // TODO: Error handling logic here.
+// Middleware for centralized and standardized error handling across the application.
+export const errorHandlingMiddleware: Middleware = () => (next) => (action) => {
+    // Check if action is rejected, and payload is a custom defined error using type guard.
+    if (isRejected(action) && isCustomDefinedError(action.payload)) {
+        switch (action.payload.statusCode) {
+            case 400:
+            break;
+            case 401:
+            break;
+            case 404:
+            break;
+            case 409:
+            break;
+            case 422:
+            break;
+            case 500:
+            break;
+            default: // For Fetch errors encountered on failed API calls.
+        }
     }
+    return next(action);
 }
