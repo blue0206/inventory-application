352a0b31b71c01e0ac8a4ffe2f8cd8194b33ee98
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-13 15:58:42 +0530

    docs: Explain the fetch logic and reasoning

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -14,6 +14,16 @@ export default function Pokemon(): ReactElement {
         types: []
     });
 
+    /**
+     * Fetches pokemon data when component mounts and whenever the pokemon ID changes.
+     * The data is stored in local component state instead of Redux state since it
+     * is not required in any other components and the amount of data is small enough
+     * to ignore memoization.
+     * 
+     * The data is fetched via thunks in order to utilize centralized error and 
+     * notification management via redux middlewares.
+     * This prevents the logic from being duplicated across multiple components.
+     */
     useEffect(() => {
         // IIFE triggered upon mount or ID change.
         ;(async function fetchData() {
