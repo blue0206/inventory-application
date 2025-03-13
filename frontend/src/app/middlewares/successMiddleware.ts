c74136cbfa4256cb8f9ae8f99680a50e3196bc26
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-13 08:03:37 +0530

    docs: Explain success middleware purpose and logic

--- frontend/src/app/middlewares/successMiddleware.ts
+++ frontend/src/app/middlewares/successMiddleware.ts
@@ -1,8 +1,16 @@
-import { Action, Middleware } from "@reduxjs/toolkit";
-import { AppStore } from "../store";
+import { isFulfilled, Middleware } from "@reduxjs/toolkit";
 
-export const successMiddleware = (store: AppStore) => (next: Middleware) => (action: Action) {
-    if (action.type.endsWith('/fulfilled')) {
-        // TODO: Handle success notifications here.
+// Middleware for showing customized notifications when an action is successful
+// and to handle any other side effects.
+export const successMiddleware: Middleware = () => (next) => (action) => {
+    // Check if action is fulfilled.
+    if (isFulfilled(action)) {
+        /**
+         * The purpose of this middleware is to show a notification on success based
+         * on the action type (via switch statement).
+         * Another purpose is to handle any side effects that need to happen after an
+         * action has been completed like redirection, other component state updates, etc.
+         */
     }
+    return next(action);
 }
