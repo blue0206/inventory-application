788f9984ac5751bb290c8ef87e45f78a47d1ed38
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-13 14:57:55 +0530

    feat: Add responsive Toaster component to AppRoutes based on media query
    See commit b3b8bd8c5ad3b5bbaa74e1dd79f186d775b60d5c for more details.

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -10,11 +10,14 @@ import {
     NotFound, 
     ErrorComponent 
 } from './components';
+import { Toaster } from 'sonner';
 import { useEffect } from 'react';
 import { navigationService } from './utils/navigation';
+import { useMediaQuery } from '@custom-react-hooks/use-media-query';
 
 export default function AppRoutes() {
     const navigate = useNavigate();
+    const isDesktop = useMediaQuery('(min-width: 768px)');
 
     // Setup navigation service to be used in redux middlewares.
     useEffect(() => {
@@ -22,20 +25,29 @@ export default function AppRoutes() {
     }, [navigate]);
 
     return (
-        <Routes>
-            <Route path='/' element={<App />} />
-            <Route path='/trainers'>
-                <Route index element={<TrainersList />} />
-                <Route path='/trainers/:trainerId' element={<Trainer />} />
-            </Route>
-            <Route path='/pokemon'>
-                <Route index element={<PokemonList />} />
-                <Route path='/pokemon/:pokemonId' element={<Pokemon />} />
-            </Route>
-            <Route path='/trainer-form' element={<TrainerForm />} />
-            <Route path='/pokemon-form' element={<PokemonForm />} />
-            <Route path='/error' element={<ErrorComponent />} />
-            <Route path='/*' element={<NotFound />} />
-        </Routes>
+        <>
+            <Routes>
+                <Route path='/' element={<App />} />
+                <Route path='/trainers'>
+                    <Route index element={<TrainersList />} />
+                    <Route path='/trainers/:trainerId' element={<Trainer />} />
+                </Route>
+                <Route path='/pokemon'>
+                    <Route index element={<PokemonList />} />
+                    <Route path='/pokemon/:pokemonId' element={<Pokemon />} />
+                </Route>
+                <Route path='/trainer-form' element={<TrainerForm />} />
+                <Route path='/pokemon-form' element={<PokemonForm />} />
+                <Route path='/error' element={<ErrorComponent />} />
+                <Route path='/*' element={<NotFound />} />
+            </Routes>
+            {
+                isDesktop ? (
+                    <Toaster richColors theme={'system'} />
+                ) : (
+                    <Toaster richColors theme={'system'} position={'top-center'} />
+                )
+            }
+        </>
     );
 }
