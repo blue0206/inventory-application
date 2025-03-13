c6e2ca8fff90188773ecf71cd66aaafb4f349c00
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-13 07:34:44 +0530

    docs: Document error handling middleware with error handling logic

--- frontend/src/app/middlewares/errorMiddleware.ts
+++ frontend/src/app/middlewares/errorMiddleware.ts
@@ -5,6 +5,21 @@ import { isCustomDefinedError } from "@/utils/custom-error";
 export const errorHandlingMiddleware: Middleware = () => (next) => (action) => {
     // Check if action is rejected, and payload is a custom defined error using type guard.
     if (isRejected(action) && isCustomDefinedError(action.payload)) {
+        /**
+         * Errors can either be CustomError or FetchError where 
+         * CustomError represents API error response data from the server while 
+         * FetchError represents network errors that occur during fetching of data. 
+         * The codebase has been designed such that the error is centralized in this middleware
+         * and the errors will ALWAYS be one of these two types. 
+         * This way we don't have to handle errors in each reducer or components, or worry about
+         * any unforeseen errors.
+         * 
+         * The switch statement will handle errors based on status codes (meaning CustomErrors) and
+         * default case for undefined status code (meaning Fetch errors).
+         * The idea here is to display an appropriate error message to the user depending on the status code.
+         * The errors messages are displayed either via a toast or by navigating to the ErrorComponent.
+         * Inside each switch case, the error handling will further be based on the action type.
+         **/
         switch (action.payload.statusCode) {
             case 400:
             break;
