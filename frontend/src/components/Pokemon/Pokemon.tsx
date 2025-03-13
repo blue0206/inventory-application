282ae460f9958b8462cbd15673ea0a24bbe31ed4
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-13 15:56:40 +0530

    feat: Fetch Pokémon details based on dynamic route parameter

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,6 +1,29 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useState } from "react";
+import { useAppDispatch } from "../../app/hooks";
+import { fetchPokemon } from "@/features/data/dataSlice";
+import { Pokemon as PokemonType } from "shared";
+import { useParams } from "react-router";
 
 export default function Pokemon(): ReactElement {
+    const dispatch = useAppDispatch();
+    const { pokemonId } = useParams();
+    const [data, setData] = useState<PokemonType>({
+        id: Number(pokemonId),
+        name: "",
+        imageLink: null,
+        types: []
+    });
+
+    useEffect(() => {
+        // IIFE triggered upon mount or ID change.
+        ;(async function fetchData() {
+            const action = await dispatch(fetchPokemon(Number(pokemonId)));
+            if (fetchPokemon.fulfilled.match(action)) {
+                setData(action.payload);
+            }
+        }());
+    }, [dispatch, pokemonId]);
+
     return (
         <></>
     );
