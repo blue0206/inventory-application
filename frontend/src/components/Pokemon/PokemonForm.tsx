ac7e7b205f3e7b9d1b19ed08eefb7667e08ee647
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-14 18:26:48 +0530

    feat: Create form for creating/updating Pokemon using Card component

--- frontend/src/components/Pokemon/PokemonForm.tsx
+++ frontend/src/components/Pokemon/PokemonForm.tsx
@@ -8,6 +8,15 @@ import {
     BreadcrumbItem,
     BreadcrumbSeparator
 } from "@/components/ui/breadcrumb";
+import {
+    Card,
+    CardHeader, 
+    CardContent, 
+    CardTitle
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { navigationService } from "../../utils/navigation";
 import { Pokemon } from "shared";
 
@@ -52,8 +61,47 @@ export default function PokemonForm({
                     </BreadcrumbList>
                 </Breadcrumb>
             </div>
-            <div>
-                
+            <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+                <div className="flex w-full max-w-sm flex-col gap-6">
+                    <div>
+                        <Card>
+                            <CardHeader>
+                                <CardTitle className="text-center text-xl font-bold">{update ? ('Update Pokémon') : ('Create Pokémon')}</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <form>
+                                    <div className="grid gap-8">
+                                        <div className="grid gap-2">
+                                            <Label htmlFor="trainerName">Pokémon Name</Label>
+                                            <Input 
+                                                type={"text"}
+                                                id={"pokemonName"} 
+                                                value={formData.name} 
+                                                onChange={(e) => setFormData({...formData, name: e.target.value})} 
+                                                placeholder="Pikachu"
+                                                required
+                                            />
+                                        </div>
+                                        <div className="grid gap-2">
+                                            <Label htmlFor="imageLink">
+                                                <span>Image Link</span>
+                                                <span className="block text-[13px] text-muted-foreground leading-none opacity-75">(Optional)</span>
+                                            </Label>
+                                            <Input 
+                                                type={'url'} 
+                                                id={"imageLink"} 
+                                                value={formData.imageLink || ""} 
+                                                onChange={(e) => setFormData({...formData, imageLink: e.target.value})} 
+                                                placeholder="URL" 
+                                            />
+                                        </div>
+                                        <Button type={"submit"} className="cursor-pointer">{update ? ("Update") : ("Create")}</Button>
+                                    </div>
+                                </form>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
             </div>
         </div>
     );
