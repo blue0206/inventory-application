3c19326729906043edf3bf8b798886e7d0f4d4ee
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-14 15:12:17 +0530

    style: Custom style MultiSelect for consistency with other components

--- frontend/src/components/MultiSelect.tsx
+++ frontend/src/components/MultiSelect.tsx
@@ -37,6 +37,40 @@ export default function MultiSelect({
                 options={options} 
                 defaultValue={defaultValue} 
                 onChange={onChange} 
+                styles={{
+                    control: (baseStyles, state) => ({
+                        ...baseStyles,
+                        borderColor: state.isFocused ? "oklch(0.655 0.016 285.938)" : "var(--input)",
+                        boxShadow: state.isFocused ? "var(--tw-inset-shadow), var(--tw-inset-ring-shadow), var(--tw-ring-offset-shadow), var(--tw-ring-inset), 0 0 0 calc(3px + var(--tw-ring-offset-width)) color-mix(in oklab, var(--ring) 50%, transparent, currentColor), var(--tw-shadow)" : "inherit",
+                        borderRadius: "var(--radius)",
+                        borderWidth: state.isFocused ? "1px" : "0.5px",
+                        borderStyle: "inherit",
+                        outlineColor: "oklch(0.21 0.006 285.885)",
+                        outline: state.isFocused ? "inherit" : "inherit",
+                        ":hover": {
+                            borderColor: state.isFocused ? "oklch(0.655 0.016 285.938)" : "var(--input)",
+                        }
+                    }),
+                    container: (baseStyles, state) => ({
+                        ...baseStyles,
+                        borderColor: state.isFocused ? "oklch(0.805 0.015 286.067)" : "var(--input)",
+                        boxShadow: state.isFocused ? "var(--tw-inset-shadow), var(--tw-inset-ring-shadow), var(--tw-ring-offset-shadow), var(--tw-ring-inset), 0 0 0 calc(3px + var(--tw-ring-offset-width)) color-mix(in oklab, var(--ring) 50%, transparent, currentColor), var(--tw-shadow)" : "inherit",
+                        borderRadius: "calc(var(--radius) + 2.5px)",
+                        borderWidth: state.isFocused ? "3px" : "0.5px",
+                        borderStyle: "inherit",
+                        outlineColor: "oklch(0.21 0.006 285.885)",
+                        outline: state.isFocused ? "3px" : "inherit",
+                        ":hover": {
+                            borderColor: state.isFocused ? "oklch(0.805 0.015 286.067)" : "var(--input)"
+                        }
+                    }),
+                }} 
+                className={
+                    `placeholder:text-muted-foreground p-0 rounded-md border bg-transparent 
+                    shadow-xs transition-[color,box-shadow] outline-none focus-visible:border-ring 
+                    focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 
+                    dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border-input`
+                }
                 required
             />
         </>
