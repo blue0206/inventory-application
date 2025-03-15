31aa293e5550bde0a6eccb9bfdd69af5154e153c
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-15 15:16:05 +0530

    feat: Create and export trainer update request type

--- frontend/src/types/requestTypes.ts
+++ frontend/src/types/requestTypes.ts
@@ -1,5 +1,13 @@
+import { TrainerRequestBody } from "shared";
+
 // Type for delete request.
 export type DeleteParamsType = {
     id: number;
     secretKey: string;
 }
+
+// Type for Trainer update request.
+export type UpdateTrainerRequestType = {
+    id: number;
+    trainerData: TrainerRequestBody;
+};
