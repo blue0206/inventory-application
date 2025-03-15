1336cfe6a6b786a3b4830e9fda63d15454061193
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-15 16:39:16 +0530

    feat: Show toast on successful create/read/update operations

--- frontend/src/app/middlewares/successMiddleware.ts
+++ frontend/src/app/middlewares/successMiddleware.ts
@@ -1,9 +1,19 @@
 import { Middleware } from "@reduxjs/toolkit";
+import { isNotDeleteAsyncThunkF } from "../reduxTypeGuard";
+import { toast } from "sonner";
 
 // Middleware for showing customized notifications when an action is successful
 // and to handle any other side effects.
 export const successMiddleware: Middleware = () => (next) => (action) => {
-    
+    if (isNotDeleteAsyncThunkF(action)) {
+        // Show success notification on successful actions.
+        // The message are returned from the server and are
+        // displayed as they are since they are already well crafted
+        // for end-user.
+        toast.success(action.payload.message, {
+            duration: 2000
+        });
+    }
     
     return next(action);
 }
