4a58f98eabb6f4ed33147ca865095f041c37dfcf
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-16 01:14:46 +0530

    feat: Remove loading toast if list fetch is success/rejected

--- frontend/src/app/middlewares/loadingMiddleware.ts
+++ frontend/src/app/middlewares/loadingMiddleware.ts
@@ -1,11 +1,13 @@
 import { Middleware } from "@reduxjs/toolkit";
 import {
+    isListGetAsyncThunkFR,
     isListGetAsyncThunkP,
 } from "../reduxTypeGuard";
 import { toast } from "sonner";
 
 export const loadingMiddleware: Middleware = () => (next) => (action) => {
 
+    // Show loading toast if pokemon or trainer list is getting fetched.
     if (isListGetAsyncThunkP(action)) {
         if (action.type.includes("pokemon")) {
             toast.loading("Fetching pokemon, please wait...", {
@@ -15,6 +17,15 @@ export const loadingMiddleware: Middleware = () => (next) => (action) => {
             toast.loading("Fetching trainers, please wait...", {
                 id: "trainerListLoading"
             });
+        }
+    }
+    // Remove loading toast if pokemon or trainer list has been successfully fetched
+    // or resulted in error.
+    if (isListGetAsyncThunkFR(action)) {
+        if (action.type.includes("pokemon")) {
+            toast.dismiss("pokemonListLoading");
+        } else {
+            toast.dismiss("trainerListLoading");
         }
     }
 
