663971f4a8f391f2802a67c223665331b294e834
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-15 15:17:11 +0530

    feat: Create async thunk to make put request updating trainer

--- frontend/src/features/form/formSlice.ts
+++ frontend/src/features/form/formSlice.ts
@@ -2,6 +2,7 @@ import { apiClient } from "@/utils/api-client";
 import { FetchError, isCustomDefinedError } from "@/utils/custom-error";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { ApiResponse, PokemonRequestBody, TrainerRequestBody } from "shared";
+import { UpdateTrainerRequestType } from "../../types/requestTypes";
 
 type FormState = {
     trainerFormLoading: boolean;
@@ -33,7 +34,10 @@ const formSlice = createSlice({
 });
 
 export const createTrainer = 
-createAsyncThunk<ApiResponse<number>, TrainerRequestBody>('form/createTrainer', async (trainerData: TrainerRequestBody, { rejectWithValue }) => {
+createAsyncThunk<
+    ApiResponse<number>, 
+    TrainerRequestBody
+>('form/createTrainer', async (trainerData: TrainerRequestBody, { rejectWithValue }) => {
     try {
         const response: ApiResponse<number> = await apiClient.createTrainer(trainerData);
         return response;
@@ -46,8 +50,28 @@ createAsyncThunk<ApiResponse<number>, TrainerRequestBody>('form/createTrainer', 
     }
 });
 
+export const updateTrainer = 
+createAsyncThunk<
+    ApiResponse<number>, 
+    UpdateTrainerRequestType
+>('form/updateTrainer', async ({ trainerData, id }: UpdateTrainerRequestType, { rejectWithValue }) => {
+    try {
+        const response: ApiResponse<number> = await apiClient.updateTrainer(trainerData, id);
+        return response;
+    } catch (error) {
+        // Actions shouldn't take class instances, hence destructure errors to object.
+        if (isCustomDefinedError(error)) {
+            return rejectWithValue({...error});
+        }
+        return rejectWithValue({...new FetchError("Something went wrong.")});
+    }
+});
+
 export const createPokemon = 
-createAsyncThunk<ApiResponse<number>, PokemonRequestBody>('form/createPokemon', async (pokemonData: PokemonRequestBody, { rejectWithValue }) => {
+createAsyncThunk<
+    ApiResponse<number>, 
+    PokemonRequestBody
+>('form/createPokemon', async (pokemonData: PokemonRequestBody, { rejectWithValue }) => {
     try {
         const response: ApiResponse<number> = await apiClient.createPokemon(pokemonData);
         return response;
