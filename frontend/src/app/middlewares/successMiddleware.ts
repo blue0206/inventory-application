4187c5ce753df79c694b2eecc7f6288d2b7c6a0d
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-15 16:28:29 +0530

    refactor: Remove switch statements to be replaced with new approach
    See a5384d6ba595528d0c93ee15e91b1202be3e53e8 and
    247bdf6f503fb56bbc58f57b3750be6fcc809825 for more information.

--- frontend/src/app/middlewares/successMiddleware.ts
+++ frontend/src/app/middlewares/successMiddleware.ts
@@ -1,60 +1,9 @@
-import { isFulfilled, Middleware } from "@reduxjs/toolkit";
-import { toast } from "sonner";
-import { navigationService } from "../../utils/navigation";
+import { Middleware } from "@reduxjs/toolkit";
 
 // Middleware for showing customized notifications when an action is successful
 // and to handle any other side effects.
 export const successMiddleware: Middleware = () => (next) => (action) => {
-    // Check if action is fulfilled.
-    if (isFulfilled(action)) {
-        /**
-         * The purpose of this middleware is to show a notification on success based
-         * on the action type (via switch statement).
-         * Another purpose is to handle any side effects that need to happen after an
-         * action has been completed like redirection, other component state updates, etc.
-         */
-        switch (action.type) {
-            // Action fetching all trainers.
-            case "trainer/fetchTrainers/fulfilled":
-                // Display info notification to create new trainer.
-                toast.info("You can add trainers from 'Create' tab!", {
-                    duration: 16000,
-                    action: {
-                        label: "Take Me!",
-                        onClick: () => navigationService.navigate("/trainer-form")
-                    },
-                    cancel: {
-                        label: "Dismiss",
-                        onClick: () => {}
-                    }
-                });
-                // Display a success notification.
-                toast.success("Fetched all trainers successfully!", {
-                    duration: 3500
-                });
-                break;
-            // Action fetching all pokemon.
-            case "pokemon/fetchPokemonList/fulfilled":
-                // Display info notification to create new pokemon.
-                toast.info("You can add Pokémon from 'Create' tab!", {
-                    duration: 16000,
-                    action: {
-                        label: "Take Me!",
-                        onClick: () => navigationService.navigate("/pokemon-form")
-                    },
-                    cancel: {
-                        label: "Dismiss",
-                        onClick: () => {}
-                    }
-                });
-                // Display a success notification.
-                toast.success("Fetched all Pokémon successfully!", {
-                    duration: 3500
-                });
-                break;
-            default:
-                
-        }
-    }
+    
+    
     return next(action);
 }
