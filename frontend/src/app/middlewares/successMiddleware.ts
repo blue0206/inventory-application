fb0be6db5a681871a1c4c6490316bf7a34c9f285
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-16 17:29:46 +0530

    refactor: Make changes preventing toast block condition
    Initially, GET action toasts were being displayed before other action
    as a GET action was made for state update after any other action.
    
    Because of this, the toast for other actions got blocked by the GET
    action toast.
    
    This commit fixes this by conditionally showing GET action toast when
    no other action toasts are pending.

--- frontend/src/app/middlewares/successMiddleware.ts
+++ frontend/src/app/middlewares/successMiddleware.ts
@@ -2,9 +2,9 @@ import { Middleware } from "@reduxjs/toolkit";
 import { 
     isCreateOrUpdatePokemonAsyncThunkF,
     isCreateOrUpdateTrainerAsyncThunkF,
-    isNotDeleteAsyncThunkF, 
+    isGetAsyncThunkF,
     isPokemonDeleteAsyncThunkF, 
-    isTrainerDeleteAsyncThunkF 
+    isTrainerDeleteAsyncThunkF
 } from "../reduxTypeGuard";
 import { toast } from "sonner";
 import { navigationService } from "@/utils/navigation";
@@ -14,53 +14,69 @@ import { resetStatus as resetPokemonListStatus } from "@/features/pokemon/pokemo
 // Middleware for showing customized notifications when an action is successful
 // and to handle any other side effects.
 export const successMiddleware: Middleware = (store) => (next) => (action) => {
-    
-    if (isNotDeleteAsyncThunkF(action)) {
-        // Show success notification on successful actions.
+
+    if (isGetAsyncThunkF(action)) {
+        // Show success notification on successful GET actions.
         // The message are returned from the server and are
         // displayed as they are since they are already well crafted
         // for end-user.
+        const toasts = toast.getToasts().map(tst => {
+            if (tst.id === "createOrUpdate" || tst.id === "deleteToast") {
+                return tst;
+            }
+        });
+        // Do not show these toasts if toasts from other actions are present.
+        if (toasts.length === 0) {
+            toast.success(action.payload.message, {
+                duration: 4000
+            });
+        }
+    }
+    if (isCreateOrUpdateTrainerAsyncThunkF(action)) {
+        // Since a trainer has been created or updated, we need to 
+        // redirect user to that trainer's detail page and show toast.
+        navigationService.navigate(`/trainers/${action.payload.data}`);
         toast.success(action.payload.message, {
-            duration: 2000
+            duration: 4000,
+            id: "createOrUpdate"
+        });
+    }
+    if (isCreateOrUpdatePokemonAsyncThunkF(action)) {
+        // Since a pokemon has been created or updated, we need to 
+        // redirect user to that pokemon's detail page and show toast.
+        navigationService.navigate(`/pokemon/${action.payload.data}`);
+        toast.success(action.payload.message, {
+            duration: 4000,
+            id: "createOrUpdate"
         });
     }
     if (isTrainerDeleteAsyncThunkF(action)) {
+        // Since a trainer has been deleted, we need to reset 
+        // the status so that a new api call is made and state is refreshed.
+        store.dispatch(resetTrainerListStatus());
+        // On delete, the user is redirected to the trainers page.
+        navigationService.navigate("/trainers");
         // Show success notification for successful delete of
         // a trainer.
         // Since the api response does not return anything in this case,
         // we use a custom message.
         toast.success("Trainer has been deleted successfully.", {
-            duration: 2000
+            duration: 4000
         });
-        // Since a trainer has been deleted, we need to reset 
-        // the status so that a new api call and state is refreshed.
-        store.dispatch(resetTrainerListStatus());
-        // On delete, the user is redirected to the trainers page.
-        navigationService.navigate("/trainers");
     }
     if (isPokemonDeleteAsyncThunkF(action)) {
-        // Show success notification for successful delete of
-        // a pokemon.
-        // Since the api response does not return anything in this case,
-        // we use a custom message.
-        toast.success("Pokemon has been deleted successfully.", {
-            duration: 2000
-        });
         // Since a pokemon has been deleted, we need to reset 
         // the status so that a new api call and state is refreshed.
         store.dispatch(resetPokemonListStatus());
         // On delete, the user is redirected to the trainers page.
         navigationService.navigate("/pokemon");
-    }
-    if (isCreateOrUpdateTrainerAsyncThunkF(action)) {
-        // Since a trainer has been created or updated, we need to 
-        // redirect user to that trainer's detail page.
-        navigationService.navigate(`/trainers/${action.payload.data}`);
-    }
-    if (isCreateOrUpdatePokemonAsyncThunkF(action)) {
-        // Since a pokemon has been created or updated, we need to 
-        // redirect user to that pokemon's detail page.
-        navigationService.navigate(`/pokemon/${action.payload.data}`);
+        // Show success notification for successful delete of
+        // a pokemon.
+        // Since the api response does not return anything in this case,
+        // we use a custom message.
+        toast.success("Pokemon has been deleted successfully.", {
+            duration: 4000
+        });
     }
     
     return next(action);
