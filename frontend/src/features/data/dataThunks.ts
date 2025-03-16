fa956086d292431073f147d9e5a13a3d07cf085e
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-16 21:52:54 +0530

    refactor: Remove redundant dataSlice redux slice
    The data slice was created to handle GET/DELETE tasks for individual
    trainer and pokemon. I was under the impression that async thunks could not be
    used without a slice but they do work.
    
    No redux state management was needed for handling the data. Only thunks
    were needed to make the requests so that middlewares could do the rest
    of the task.
    
    Therefore, the module itself has been named to `dataThunks.ts` since all
    it does is store async thunks.
    
    The slice was redundant here as there was no state management being
    performed and hence has been removed.

--- frontend/src/features/data/dataThunks.ts
+++ frontend/src/features/data/dataThunks.ts
@@ -1,39 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { createAppAsyncThunk } from "@/app/hooks";
-import { FetchError, isCustomDefinedError } from "@/utils/custom-error";
+import { createAppAsyncThunk } from "../../app/hooks";
+import { FetchError, isCustomDefinedError } from "../../utils/custom-error";
 import { apiClient } from "../../utils/api-client";
 import { Pokemon, TrainerWithRelation, ApiResponse } from "shared";
 import { DeleteParamsType } from "../../types/requestTypes";
 
-type Status = "idle" | "loading" | "succeeded" | "failed";
-type DataState = {
-    trainerDataStatus: Status;
-    pokemonDataStatus: Status;
-}
-
-const initialState: DataState = {
-    trainerDataStatus: 'idle',
-    pokemonDataStatus: 'idle'
-}
-
 /**
- * The purpose of this slice is to handle thunks regarding specific
+ * The purpose of this module is to store thunks regarding specific
  * tasks like getting specific trainer/pokemon data in order to display
  * them on a separate dynamic route. The actual data pertaining to 
  * trainer/pokemon is not required to be globally available and hence there
- * is no Redux state created to hold them.
+ * is no Redux state/slice created to hold them.
  * 
- * The reason this slice has been created and the fetching of data is not
+ * The reason these thunks have been created and the fetching of data is not
  * being handled in local component is because the error handling
  * and notifications showcase has been standardized and centralized via redux 
  * middlewares and to trigger that, a request made via async thunk
  * is imperative.
  */
-const dataSlice = createSlice({
-    name: 'data',
-    initialState,
-    reducers: {}
-});
 
 export const fetchTrainer = 
 createAppAsyncThunk<
@@ -90,5 +73,3 @@ createAppAsyncThunk<void, DeleteParamsType>('data/deletePokemon', async ({ id, d
         return rejectWithValue({...new FetchError("Something went wrong.")});
     }
 });
-
-export default dataSlice.reducer;
