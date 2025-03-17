4c60935337025383fc27cae120a03762c8e261ac
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-03-17 14:10:55 +0530

    feat: Show skeleton loading animation while pokemon list loads

--- frontend/src/components/Pokemon/PokemonList.tsx
+++ frontend/src/components/Pokemon/PokemonList.tsx
@@ -3,11 +3,14 @@ import { Header } from "..";
 import PokemonCard from "./PokemonCard";
 import { useAppSelector, useAppDispatch } from "@/app/hooks";
 import { fetchPokemonList, getPokemonList, getStatus, resetStatus } from "@/features/pokemon/pokemonSlice";
+import SkeletonCard from "../Skeleton/SkeletonCard";
 
 export default function PokemonList(): ReactElement {
     const dispatch = useAppDispatch();
     const pokemon = useAppSelector(getPokemonList);
     const status = useAppSelector(getStatus);
+    // Array used in skeleton loading animation
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
     useEffect(() => {
         // Fetch pokemon if status is 'idle'
@@ -28,18 +31,27 @@ export default function PokemonList(): ReactElement {
             <Header />
             <div className="grid grid-cols-1 px-5 gap-8 sm:grid-cols-2 lg:grid-cols-4">
                 {
-                    pokemon.length > 0 ? (
-                        pokemon.map(poke => {
+                    status === "loading" ? (
+                        arr.map(iter => {
                             return (
-                                <PokemonCard 
-                                    key={poke.id} 
-                                    id={poke.id} 
-                                    name={poke.name} 
-                                    image={poke.imageLink}
-                                />
+                                <SkeletonCard key={iter} />
                             )
                         })
-                    ) : (null)
+                    ) : (
+
+                        pokemon.length > 0 ? (
+                            pokemon.map(poke => {
+                                return (
+                                    <PokemonCard 
+                                        key={poke.id} 
+                                        id={poke.id} 
+                                        name={poke.name} 
+                                        image={poke.imageLink}
+                                    />
+                                )
+                            })
+                        ) : (null)
+                    )
                 }
             </div>
         </div>
